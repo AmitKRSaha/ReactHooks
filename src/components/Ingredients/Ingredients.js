cdb2211476937c85ffd43c8c667d117f6677d502
AKSAHA <sheol.aks@gmail.com>
2020-08-16 19:53:35 +0530

    added debounce

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,9 +3,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 function Ingredients() {
   let [userIng, setUserIng] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
   // useEffect(() => {
   //   async function loaddata() {
@@ -21,28 +24,46 @@ function Ingredients() {
   // }, []);
 
   const addIngredients = async (ingredients) => {
+    setLoading(true);
     const res = await fetch('https://rc-hooks.firebaseio.com/ingredients.json',
       {
         method: 'POST', body: JSON.stringify(ingredients), headers: { 'Content-Type': 'application/json' }
       });
     const data = await res.json();
     setUserIng(prevIng => [...prevIng].concat({ id: data.name, ...ingredients }));
+    setLoading(false);
   }
 
-  const removeIngredients = (id) => {
-    setUserIng(prevIng => prevIng.filter(ing => ing.id !== id));
+  const removeIngredients = async (id) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`https://rc-hooks.firebaseio.com/ingredients/${id}.json`,
+        {
+          method: 'DELETE'
+        });
+      const data = await res.json();
+      setUserIng(prevIng => prevIng.filter(ing => ing.id !== id));
+      setLoading(false);
+    } catch (err) {
+      setError('Something is wrong');
+    }
   }
 
   const onLoadIngredients = useCallback((ingre) => {
     setUserIng(ingre);
-    console.log('Ingre' + JSON.stringify(ingre));
+    // console.log('Ingre' + JSON.stringify(ingre));
   }, []);
+
+  const clearError = () => {
+    setError(null);
+    setLoading(false);
+  }
 
 
   return (
     <div className="App">
-      <IngredientForm addIngredients={addIngredients} />
-
+      <IngredientForm addIngredients={addIngredients} loading={loading} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <section>
         <Search onLoadIngredients={onLoadIngredients} />
         {/* Need to add list here! */}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -8,30 +8,40 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;
 
   const [searchState, setSearchState] = useState('');
+  const inputRef = useRef('');
 
   useEffect(() => {
-    async function loaddata() {
-      const query = searchState.length === 0 ? '' : `?orderBy="title"&equalTo="${searchState}"`;
-      const res = await fetch('https://rc-hooks.firebaseio.com/ingredients.json' + query);
-      const data = await res.json();
-      console.log(data);
-      const loadedData = [];
-      for (let d in data) {
-        loadedData.push({ id: d, title: data[d].title, amount: data[d].amount })
-        // setUserIng(prevIng => [...prevIng].concat({ id: d, title: data[d].title, amount: data[d].amount }));
+    const timer = setTimeout(() => {
+      if (searchState === inputRef.current.value) {
+        async function loaddata() {
+          const query = searchState.length === 0 ? '' : `?orderBy="title"&equalTo="${searchState}"`;
+          const res = await fetch('https://rc-hooks.firebaseio.com/ingredients.json' + query);
+          const data = await res.json();
+          // console.log(data);
+          const loadedData = [];
+          for (let d in data) {
+            loadedData.push({ id: d, title: data[d].title, amount: data[d].amount })
+            // setUserIng(prevIng => [...prevIng].concat({ id: d, title: data[d].title, amount: data[d].amount }));
+          }
+          // return data;
+          onLoadIngredients(loadedData);
+        }
+        loaddata();
       }
-      // return data;
-      onLoadIngredients(loadedData);
-    }
-    loaddata();
-  }, [searchState, onLoadIngredients]);
+
+    }, 500);
+
+    // Cleaning up the old timer for each new search item
+    return () => { clearTimeout(timer) };
+
+  }, [searchState, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={searchState}
+          <input type="text" value={searchState} ref={inputRef}
             onChange={(evt) => setSearchState(evt.target.value)} />
         </div>
       </Card>
